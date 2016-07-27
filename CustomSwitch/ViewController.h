fda7d2ac2169ae2e196e7754998a6b270144320b
Constantine Mars <constantine.mars@gmail.com>
2016-07-27 18:02:13 +0300

    Initial Commit

---
+++ CustomSwitch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomSwitch
+//
+//  Created by Constantine Mars on 7/27/16.
+//  Copyright © 2016 HealthPrize. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomSwitch/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CustomSwitch
+//
+//  Created by Constantine Mars on 7/27/16.
+//  Copyright © 2016 HealthPrize. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
