a28485b8a20dc157d91f6f4984b84e6ec4bab169
Constantine Mars <constantine.mars@gmail.com>
2016-07-27 18:29:43 +0300

    selected

--- CustomSwitch/ViewController.m
+++ CustomSwitch/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *button;
 
 @end
 
@@ -16,12 +17,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self.button setImage:[UIImage imageNamed:@"checkbox_on"] forState:UIControlStateSelected];
+    [self.button setImage:[UIImage imageNamed:@"checkbox_off"] forState:UIControlStateNormal];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)toggle:(id)sender {
+    self.button.selected = ! self.button.selected;
+    NSLog(@"%d", self.button.isSelected);
 }
 
 @end
